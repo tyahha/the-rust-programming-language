8ee0dff85ea49c1af50f19fc5362dfb4bdc03edb
toshinori.sato <tyahhahha@gmail.com>
2025-12-27 22:44:19 +0900

    not to use deprecated methods in rand crate

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,7 +4,7 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let secret_number = rand::rng().random_range(1..101);
 
     println!("The secret number is: {}", secret_number);
 
