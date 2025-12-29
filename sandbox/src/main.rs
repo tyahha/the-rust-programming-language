34196263e840420c237b7a5727edbbfe7bca3f07
toshinori.sato <tyahhahha@gmail.com>
2025-12-29 23:02:05 +0900

    cloneの実験

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -7,4 +7,8 @@ fn main() {
     // let str_x = String::from("hello");
     // let str_y = str_x;
     // println!("str_x: {}, str_y: {}", str_x, str_y);
+
+    let str_a = String::from("hello");
+    let str_b = str_a.clone();
+    println!("str_a: {}, str_b: {}", str_a, str_b);
 }
