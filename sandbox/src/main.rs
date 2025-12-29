f9246c21da1d809a402d1fb4cf769ae259d35d29
toshinori.sato <tyahhahha@gmail.com>
2025-12-29 23:06:55 +0900

    関数に渡すことで所有権がなくなることを確認

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,14 +1,24 @@
 fn main() {
-    let num_x = 3;
-    let num_y = num_x;
-    println!("num_x: {}, num_y: {}", num_x, num_y);
+    // let num_x = 3;
+    // let num_y = num_x;
+    // println!("num_x: {}, num_y: {}", num_x, num_y);
 
+    // // error: value borrowed here after move
+    // // let str_x = String::from("hello");
+    // // let str_y = str_x;
+    // // println!("str_x: {}, str_y: {}", str_x, str_y);
+
+    // let str_a = String::from("hello");
+    // let str_b = str_a.clone();
+    // println!("str_a: {}, str_b: {}", str_a, str_b);
+
+    let s = String::from("hello");
+    print!("Before function call: {}\n", s);
+    takes_ownership(s);
     // error: value borrowed here after move
-    // let str_x = String::from("hello");
-    // let str_y = str_x;
-    // println!("str_x: {}, str_y: {}", str_x, str_y);
+    // print!("After function call: {}\n", s);
+}
 
-    let str_a = String::from("hello");
-    let str_b = str_a.clone();
-    println!("str_a: {}, str_b: {}", str_a, str_b);
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
 }
