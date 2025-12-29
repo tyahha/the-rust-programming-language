1210669855389936262da30b0187620b85aa5112
toshinori.sato <tyahhahha@gmail.com>
2025-12-29 22:45:19 +0900

    experiment of borrowing

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let num_x = 3;
+    let num_y = num_x;
+    println!("num_x: {}, num_y: {}", num_x, num_y);
+
+    // error: value borrowed here after move
+    // let str_x = String::from("hello");
+    // let str_y = str_x;
+    // println!("str_x: {}, str_y: {}", str_x, str_y);
 }
