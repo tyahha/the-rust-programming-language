a83ec6c168e8147ec72245bed444792d6512d8fa
toshinori.sato <tyahhahha@gmail.com>
2025-12-30 23:26:10 +0900

    メソッドを利用する

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -4,12 +4,14 @@ struct Rectangle {
     height: u32,
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
     println!("rect1 is {:?}", rect1);
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
 }
