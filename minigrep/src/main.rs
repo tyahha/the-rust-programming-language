2f756bcf06f60ab6567c5d60950ffac1be31c4a9
toshinori.sato <tyahhahha@gmail.com>
2026-01-06 00:30:03 +0900

    インスタンスに関連関数を利用する

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("In file {}", config.filename);
 
@@ -17,8 +17,10 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
