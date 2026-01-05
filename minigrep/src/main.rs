2dd016a5ede9641179954b94d183abfc6f26b69c
toshinori.sato <tyahhahha@gmail.com>
2026-01-06 00:31:41 +0900

    引数不足のエラーを表示する

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -19,6 +19,10 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
         Config { query, filename }
