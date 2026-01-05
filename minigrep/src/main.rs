719bd98b9a1b0d4e2e9f1bd605a1be9b1e80b11d
toshinori.sato <tyahhahha@gmail.com>
2026-01-06 00:20:10 +0900

    ファイルの内容を読み取る

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,14 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("In file {}", filename);
+
+    let f = std::fs::File::open(filename).expect("File not found");
+
+    let contents = std::io::read_to_string(f).expect("Something went wrong reading the file");
+
+    println!("With query {}", contents);
 }
